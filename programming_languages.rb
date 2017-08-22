2971644720404d415f4d9499cd1b597903688453
Cole Shapiro <colesayershapiro@gmail.com>
2017-08-22 17:42:35 +0000

    Done.

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,20 @@
+#check to see if the new_language_hash[name] already exists and if it does,
+#add relevant information to the language data
+
 def reformat_languages(languages)
-  # your code here
+  new_language_hash = {}
+  languages.each do |style, language|
+    language.each do |name, data|
+      data.each do |type, value|
+        if new_language_hash[name]
+          new_language_hash[name][:style] << style
+        else
+        new_language_hash[name] = {type: value, style: [style]}
+
+
+      end
+      end
+    end
+  end
+  new_language_hash
 end
